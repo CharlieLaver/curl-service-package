c5faff85f26bf2d807633487a80b2565993c9643
clvr7 <charlielaver7@gmail.com>
2021-12-21 13:30:56 +0000

    Update REST.php

--- src/REST.php
+++ src/REST.php
@@ -5,7 +5,7 @@ namespace Clvr7\ApiClient;
 class REST extends ApiClientBase {
 
     public static function get($endpoint) {
-		$ch = self::curlOpts("GET", $endpoint);
+	$ch = self::curlOpts("GET", $endpoint);
         $response = curl_exec($ch);
         if($e = curl_error($ch)) {
             return self::formatReturnData($e, false);
@@ -16,7 +16,7 @@ class REST extends ApiClientBase {
     }
 
     public static function post($endpoint, $data) {
-		$ch = self::curlOpts("POST", $endpoint, $data);
+	$ch = self::curlOpts("POST", $endpoint, $data);
         $response = curl_exec($ch);
         if($e = curl_error($ch)) {
             return self::formatReturnData($e, false);
@@ -27,7 +27,7 @@ class REST extends ApiClientBase {
     }
 
 	public static function put($endpoint, $data) {
-		$ch = self::curlOpts("PUT", $endpoint, $data);
+	$ch = self::curlOpts("PUT", $endpoint, $data);
         $response = curl_exec($ch);
         if($e = curl_error($ch)) {
             return self::formatReturnData($e, false);
