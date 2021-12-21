868af7cba80faf2f66c395909fbeae7aa9f28016
Charlie Laver <charlielaver7@gmail.com>
2021-12-21 13:09:16 +0000

    post req method in REST

--- src/REST.php
+++ src/REST.php
@@ -16,5 +16,20 @@ class REST extends ApiClientBase {
         }
         curl_close();
     }
+
+    public static function post($endpoint, $data) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $endpoint);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        if($e = curl_error($ch)) {
+            return self::formatReturnData($e, false);
+        } else {
+            return self::formatReturnData($response, true);
+        }
+        curl_close();
+    }
  
 }
